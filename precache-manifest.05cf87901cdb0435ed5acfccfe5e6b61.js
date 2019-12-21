6c1331f9a840fd2bd8d36184ca2bac9f6c3f51dc
amitshekatkar <shekatkaramit@gmail.com>
2019-12-21 14:27:19 +0530

    Updates

---
+++ precache-manifest.05cf87901cdb0435ed5acfccfe5e6b61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "686e6db1412e8a7d8180",
+    "url": "/static/js/main.6ffcd9be.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "686e6db1412e8a7d8180",
+    "url": "/static/css/main.cbebc642.chunk.css"
+  },
+  {
+    "revision": "95b96d2fe67a3f913542a3e521c4cd7f",
+    "url": "/index.html"
+  }
+];
